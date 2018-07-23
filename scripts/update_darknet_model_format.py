9ea3d004522c886c515a00412bca7dde4e83a51c
Tadashi MATSUO <matsuo@i.ci.ritsumei.ac.jp>
2018-07-23 20:06:46 +0900

    Add scripts for converting model files of Darknet.

---
+++ scripts/update_darknet_model_format.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import argparse
+import ctypes
+import sys
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Update format of a darknet model by loading/saving it.'
+    )
+    parser.add_argument('--darknet_library', default='libdarknet.so')
+    parser.add_argument('--input_model', default='yolov2.weights')
+    parser.add_argument('--input_cfg', default='yolov2.cfg')
+    parser.add_argument('--output_model', default='yolov2_new.weights')
+
+    parsed_args = sys.argv
+    try:
+        import rospy
+        parsed_args = rospy.myargv(argv=sys.argv)
+    except ImportError:
+        pass
+
+    args = parser.parse_args(parsed_args[1:])
+
+    lib = ctypes.CDLL(args.darknet_library, ctypes.RTLD_GLOBAL)
+    load_net = lib.load_network
+    load_net.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
+    load_net.restype = ctypes.c_void_p
+
+    save_weights = lib.save_weights
+    save_weights.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+
+    net = load_net(args.input_cfg, args.input_model, 0)
+    save_weights(net, args.output_model)
