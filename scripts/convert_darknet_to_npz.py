9d6e57e6c84894ed5026d7b699026c92cd7ce397
Tadashi MATSUO <matsuo@i.ci.ritsumei.ac.jp>
2018-08-01 19:39:36 +0900

    'scripts/convert_darknet_to_npz.py' can read a darknet model in old formats.

--- scripts/convert_darknet_to_npz.py
+++ scripts/convert_darknet_to_npz.py
@@ -4,6 +4,7 @@
 #
 import argparse
 import numpy as np
+import sys
 
 import chainer
 from chainer.links import Convolution2D
@@ -62,48 +63,134 @@ def load_yolo_v3(file, model):
     for subnet in model.subnet:
         reorder_loc(subnet[-1], model.n_fg_class)
 
+def read_version_of_format(f):
+    major = np.fromfile(f, dtype=np.int32, count=1)
+    minor = np.fromfile(f, dtype=np.int32, count=1)
+    return major, minor
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--model', choices=('yolo_v2', 'yolo_v3'),
-        default='yolo_v2')
-    parser.add_argument('--n-fg-class', type=int, default=80)
-    parser.add_argument('darknetmodel')
-    parser.add_argument('output')
-    args = parser.parse_args()
+def update_format_of_darknet_model(output_filename, input_filename, darknet_library, darknet_cfg_file):
+    import ctypes
+    try:
+        lib = ctypes.CDLL(darknet_library, ctypes.RTLD_GLOBAL)
+    except OSError as err:
+        print('OS error: %s' % (err,))
+        print('Failed to import the darknet library "%s"' % (darknet_library,))
+        return False
 
-    if args.model == 'yolo_v2':
+    load_net = lib.load_network
+    load_net.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
+    load_net.restype = ctypes.c_void_p
+
+    save_weights = lib.save_weights
+    save_weights.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+
+    net = load_net(darknet_cfg_file, input_filename, 0)
+    save_weights(net, output_filename)
+    return True
+
+def load_darknet_model(filename, model_type, number_of_foreground_classes, retry_with_update_if_possible=False, darknet_library='libdarknet.so',  darknet_cfg_filename=''):
+    if model_type == 'yolo_v2':
         model = make_serializable_object(
             chainercv.links.YOLOv2,
             constructor_args = {
-                'n_fg_class': args.n_fg_class,
+                'n_fg_class': number_of_foreground_classes,
             }
         )
-    elif args.model == 'yolo_v3':
+    elif model_type == 'yolo_v3':
         model = make_serializable_object(
             chainercv.links.YOLOv3,
             constructor_args = {
-                'n_fg_class': args.n_fg_class,
+                'n_fg_class': number_of_foreground_classes,
             }
         )
 
     with chainer.using_config('train', False):
         model(np.empty((1, 3, model.insize, model.insize), dtype=np.float32))
 
-    with open(args.darknetmodel, mode='rb') as f:
+    model_is_old_format = False
+    with open(filename, mode='rb') as f:
         major = np.fromfile(f, dtype=np.int32, count=1)
         minor = np.fromfile(f, dtype=np.int32, count=1)
         np.fromfile(f, dtype=np.int32, count=1)  # revision
-        assert(major * 10 + minor >= 2 and major < 1000 and minor < 1000)
-        np.fromfile(f, dtype=np.int64, count=1)  # seen
+        if major == 0 and minor <= 1:
+            model_is_old_format = True
+            print(
+                'The file "%s" is written in an old format, where (major, minor) == (%d, %d) '
+                % (filename, major, minor)
+            )
+            sys.stdout.flush()
+            model = None
+        else:
+            print(
+                'The file "%s" is written in a new format, where (major, minor) == (%d, %d) '
+                % (filename, major, minor)
+            )
+            assert(major * 10 + minor >= 2 and major < 1000 and minor < 1000)
+            model_is_old_format = False
+            np.fromfile(f, dtype=np.int64, count=1)  # seen
+            if model_type == 'yolo_v2':
+                load_yolo_v2(f, model)
+            elif model_type == 'yolo_v3':
+                load_yolo_v3(f, model)
 
-        if args.model == 'yolo_v2':
-            load_yolo_v2(f, model)
-        elif args.model == 'yolo_v3':
-            load_yolo_v3(f, model)
+    if model_is_old_format and retry_with_update_if_possible:
+        import tempfile
+        import os
+        print(
+            'Try to convert "%s" to the new format and load it.'
+            % (filename,)
+        )
+        sys.stdout.flush()
+        fileno, tmp_path = tempfile.mkstemp()
+        try:
+            succeeded = update_format_of_darknet_model(
+                tmp_path, filename, darknet_library, darknet_cfg_filename
+            )
+            if succeeded:
+                print('Succeeded to update "%s" and save the updated version into "%s".'
+                      % (filename, tmp_path))
+                sys.stdout.flush()
+                model = load_darknet_model(
+                    tmp_path, model_type, number_of_foreground_classes,
+                    retry_with_update_if_possible=False
+                )
+            else:
+                print('Failed to convert "%s".' % (filename,))
+                sys.stdout.flush()
+        finally:
+            # remove the temporary file
+            os.remove(tmp_path)
+    return model
 
-    save_npz_with_structure(args.output, model)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model_type', choices=('yolo_v2', 'yolo_v3'),
+        default='yolo_v2')
+    parser.add_argument('--n_fg_class', type=int, default=80)
+    parser.add_argument('--darknet_library', default='libdarknet.so')
+    parser.add_argument('--input_darknet_model', default='yolov2.weights')
+    parser.add_argument('--input_darknet_cfg', default='yolov2.cfg')
+    parser.add_argument('--output')
+    args = parser.parse_args()
+
+    input_model_filename = args.input_darknet_model
+    input_cfg_filename = args.input_darknet_cfg
+    output_filename = args.output
+    print('Loading a darknet model from "%s".' % (input_model_filename, ))
+    sys.stdout.flush()
+    model = load_darknet_model(
+        input_model_filename, args.model_type, args.n_fg_class,
+        retry_with_update_if_possible=True,
+        darknet_library=args.darknet_library,
+        darknet_cfg_filename=args.input_darknet_cfg,
+    )
+    if model:
+        print('Saving a darknet model to "%s".' % (output_filename, ))
+        save_npz_with_structure(output_filename, model)
+    else:
+        print('Failed to load a model from "%s".' % (input_model_filename, ))
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
